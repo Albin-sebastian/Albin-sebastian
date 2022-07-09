92030337d72665b2d28dee7ae07fa5a5ce92be54
Albin Sebastian <albinsebastrian@gmail.com>
2022-07-09 18:15:40 +0530

    comitted

---
+++ build/precache-manifest.8103a10bd547b9a18c35c58fdf6da403.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dc7cc34ebd61e1abf01",
+    "url": "/static/js/main.0dc7cc34.chunk.js"
+  },
+  {
+    "revision": "f72a658f94b04f2575a2",
+    "url": "/static/js/1.f72a658f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7096e563f3c987e8ccf14fc549d77d04",
+    "url": "/index.html"
+  }
+];
